8db45faec941043449730b869de2ef26f4ee4dba
justinanazor <anazorjustin@gmail.com>
2023-08-25 12:14:07 +0100

    added header.js and modified more js

--- js/first.js
+++ js/first.js
@@ -1,5 +1,19 @@
-var namei = "john doe";
-console.log(namei);
-console.log(namei);
-var man = "Jay";
-console.log(man);
+// var namei = "john doe";
+// console.log(namei);
+// console.log(namei);
+// var man = "Jay";
+// console.log(man);
+
+function clicker() {
+  // document.getElementById("btnp").innerHTML = "clicked";
+  alert("wow it worked!");
+}
+
+function hv() {
+  document.getElementById("hove").style.color = "pink";
+  document.getElementById("hove").style.backgroundColor = "black";
+}
+function hv1() {
+  document.getElementById("hove").style.color = "black";
+  document.getElementById("hove").style.backgroundColor = "white";
+}
